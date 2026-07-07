98b14623f24eb6439405eaf22b6120f79fd308cd
JD <mcrtu01@gmail.com>
2026-07-07 01:55:17 -0500

    test: Implementar Golden Data para estabilizar tests End-to-End de Newman

--- FraudGuard.API/Controllers/TestingController.cs
+++ FraudGuard.API/Controllers/TestingController.cs
@@ -25,24 +25,30 @@ public class TestingController : ControllerBase
         _context.BlockedEntities.RemoveRange(_context.BlockedEntities);
         await _context.SaveChangesAsync();
 
-        // 2. Bogus: Generamos 50 usuarios falsos con países aleatorios
+        // 2. GOLDEN DATA: Insertar los datos exactos que espera Postman/Newman
+        var qaUser = new User { Id = Guid.Parse("3fa85f64-5717-4562-b3fc-2c963f66afa6"), CountryOfResidence = "PE" };
+        var qaMerchant = new Merchant { Id = Guid.Parse("3fa85f64-5717-4562-b3fc-2c963f66afa6"), Name = "QA Automation Co", IsActive = true };
+        await _context.Users.AddAsync(qaUser);
+        await _context.Merchants.AddAsync(qaMerchant);
+
+        // 3. Bogus: Generar 50 usuarios falsos con países aleatorios
         var userFaker = new Faker<User>()
             .RuleFor(u => u.Id, f => Guid.NewGuid())
             .RuleFor(u => u.CountryOfResidence, f => f.Address.CountryCode());
         var fakeUsers = userFaker.Generate(50);
 
-        // 3. Bogus: Generamos 10 comercios
+        // 4. Bogus: Generar 10 comercios
         var merchantFaker = new Faker<Merchant>()
             .RuleFor(m => m.Id, f => Guid.NewGuid())
             .RuleFor(m => m.Name, f => f.Company.CompanyName())
             .RuleFor(m => m.IsActive, true);
         var fakeMerchants = merchantFaker.Generate(10);
 
-        // 4. Guardamos en SQL Server
+        // 4. Guardar en SQL Server
         await _context.Users.AddRangeAsync(fakeUsers);
         await _context.Merchants.AddRangeAsync(fakeMerchants);
         await _context.SaveChangesAsync();
 
-        return Ok(new { Message = "Base de datos reseteada y sembrada con éxito", Users = 50, Merchants = 10 });
+        return Ok(new { Message = "DB reseteada y sembrada con éxito"});
     }
 }
